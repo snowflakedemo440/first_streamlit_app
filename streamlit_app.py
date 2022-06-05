90a28dcd1b1da8b1ed1bb04c925722605dae66b1
snowflake440 <106872103+snowflakedemo440@users.noreply.github.com>
2022-06-04 19:25:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 #let’s put a pick list here so they can pick the fruit they want to include.
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index())
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index()))
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
